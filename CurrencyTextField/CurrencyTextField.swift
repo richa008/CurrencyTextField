1f875c5eace02c7b02a9545e4671d5e5c72561c4
Deshmukh,Richa <Richa.Deshmukh@Cerner.com>
2016-06-03 19:49:13 +0530

    Adding currency text field and delegate

--- CurrencyTextField/CurrencyTextField.swift
+++ CurrencyTextField/CurrencyTextField.swift
@@ -7,3 +7,42 @@
 //
 
 import Foundation
+import UIKit
+
+@IBDesignable public class CurrencyTextField : UITextField{
+    
+    let maxDigits = 12
+    
+    let currencyTextFieldDelegate = CurrencyTextFieldDelegate()
+    
+    let currencyFormattor = NSNumberFormatter()
+    
+    // MARK: - init functions
+    
+    override public init(frame: CGRect) {
+        super.init(frame: frame)
+        initTextField()
+    }
+
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        initTextField()
+    }
+    
+    func initTextField(){
+        self.keyboardType = UIKeyboardType.DecimalPad
+        self.text = "$0.00"
+        self.delegate = currencyTextFieldDelegate
+        currencyFormattor.numberStyle = .CurrencyStyle
+        currencyFormattor.minimumFractionDigits = 2
+        currencyFormattor.maximumFractionDigits = 2
+    }
+    
+    //MARK: - Custom text field functions
+    
+    func setAmount (amount : Double){
+        let textFieldStringValue = currencyFormattor.stringFromNumber(amount)
+        self.text = textFieldStringValue
+    }
+    
+}
--- CurrencyTextFieldDemo/CurrencyTextFieldDemo/ViewController.swift
+++ CurrencyTextFieldDemo/CurrencyTextFieldDemo/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: CurrencyTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
